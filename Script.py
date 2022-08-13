6e6c467649e321c6261a795d0b7b72a033a0e6b7
Strange-AR <109451779+Strange-AR@users.noreply.github.com>
2022-08-13 17:08:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -129,11 +129,19 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """
+    <b>➠ Current Status of Jᴇʀʀʏ⚡️</b>
+
+    <b>★ Total Files: <code>{}</code></b>
+
+    <b>★ Total Users: <code>{}</code></b>
+
+    <b>★ Total Chats: <code>{}</code></b>
+
+    <b>★ Used Storage: <code>{}</code></b>
+
+    <b>★ Free Storage : <code>{}</code></b>
+    """
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
