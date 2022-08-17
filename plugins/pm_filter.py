2a60bdce36ec60b66a20bf9ee8efbadd8fab07c7
Strange-AR <109451779+Strange-AR@users.noreply.github.com>
2022-08-17 23:31:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -861,9 +861,10 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+    await msg.reply("<b>I Couldn't Find Anything Related To That!! Did You Mean Any One of These?",
                     reply_markup=InlineKeyboardMarkup(btn))
-
+    await asyncio.sleep(10)
+    await k.delete()
 
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
