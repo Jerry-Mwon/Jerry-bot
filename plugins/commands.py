019d93409d876be2d7f8c03c14385f461ffb0420
AR <109451779+Strange-AR@users.noreply.github.com>
2022-08-24 18:46:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -258,8 +258,7 @@ async def start(client, message):
       )
         await asyncio.sleep(DELETE_TIME)
     await sk.delete()
-        protect_content=True if pre == 'filep' else False,
-        )
+       
                     
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
