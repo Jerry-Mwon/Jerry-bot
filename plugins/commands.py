aa13605c16d2e2e45537aa609346c6515a04ad95
AR <109451779+Strange-AR@users.noreply.github.com>
2022-09-11 03:45:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -250,17 +250,14 @@ async def start(client, message):
         except Exception as e:
             logger.exception(e)
             f_caption=f_caption
-        #sk = await client.send_cached_media(
+        if f_caption is None:
+        f_caption = f"{files.file_name}"
+    await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
-      #)
-        await asyncio.sleep(DELETE_TIME)
-    await sk.delete()
-    protect_content=True if pre == 'filep' else False,
-                  
-                    
+        )
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
